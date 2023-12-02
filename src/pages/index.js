8705e9fd12065d38146df4b3d4f29b76f4b6c3ae
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-12-02 12:40:10 -0800

    Added Landing styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,54 +1,6 @@
 import Image from "next/image";
-import {
-  motion,
-  useMotionValue,
-  useTransform,
-  AnimatePresence,
-  easeOut,
-} from "framer-motion";
-const firstName = ["N", "I", "C", "K", "S", "H", "U", "M", "."];
-const lastName = ["S", "H", "U", "M", "."];
-
-const slideUpFirst = {
-  initial: {
-    y: 0,
-  },
-  enter: (index) => ({
-    y: -150,
-    transition: { 
-      delay: (index * 0.03) +1, 
-      duration: 1,
-      ease: [0.22, 1, 0.36, 1]
-    },
-  }),
-  exit: {
-    y: 0,
-  },
-};
-const slideUpSecond = {
-  initial: {
-    y: 0,
-  },
-  enter: (index) => ({
-    y: -150,
-    x: index === 0 ? 260 : index === 8 ? -360 : 0,
-    transition: { 
-      delay: (index * 0.03) +1, 
-      duration: 1,
-      ease: [0.22, 1, 0.36, 1],
-      x: {
-        delay: 1.9,
-        duration: 1,
-        ease: [0.68, -0.6, 0.32, 1.6],
-      }
-      
-    },
-  }),
-  exit: {
-    y: 0,
-  },
-};
-
+import { motion } from "framer-motion";
+import { Loading } from "@/sections";
 
 const slideDownLast = {
   initial: {
@@ -70,63 +22,33 @@ const slideDownLast = {
 export default function Home() {
   return (
     <>
-    <div className="relative z-0 h-[100dvh] w-[100dvw] flex flex-col justify-center items-center bg-secondary"
-    >
-      <div className="h-[164px] overflow-y-clip flex-col flex justify-start items-center">
-        <div className="text-primary uppercase flex font-bold text-[150px] leading-[150px]">
-          {firstName.map((letter, index) => (
-            <motion.div
-              key={index}
-              custom={index}
-              variants={slideUpFirst}
-              initial="initial"
-              animate="enter"
-              exit="exit"
-            >
-              {letter}
-            </motion.div>
-          ))}
-  
+      <div className="flex  flex-col w-full justify-between h-[100svh] bg-primary p-10 relative">
+        <div className="flex z-10 flex-col justify-start items-start w-full">
+          <div className="text-[150px] leading-[100px] font-bold tracking-tight text-tertiary">
+            SOFTWARE
+          </div>
+          <div className="text-[150px] leading-[100px] mt-7 font-bold tracking-tight text-tertiary">
+              DEVELOPER
+          </div>
+          <div className="flex justify-start mt-3 items-center gap-4 w-full">
+            <div className="bg-tertiary h-[4px] w-[80px] ml-3"/>
+            <div className="text-tertiary text-[45px] font-medium">UNIVERSITY STUDENT</div>
+          </div>
         </div>
-
-        <div className="text-primary uppercase flex font-bold text-[150px] leading-[150px]">
-          {firstName.map((letter, index) => (
-            <motion.span
-              key={index}
-              custom={index}
-              variants={slideUpSecond}
-              initial="initial"
-              animate="enter"
-              exit="exit"
-              className={`${index === 0 || index === 4 || index === 8 ? "" : "opacity-0"}`}
-            >
-              {letter}
-            </motion.span>
-          ))}
-          {/* {lastName.map((letter, index) => (
-            <motion.div
-              key={index}
-                           custom={index}
-              variants={slideUpLast}
-              initial="initial"
-              animate="enter"
-              exit="exit"
-              className={`${index === 0 ? "ml-[40px]" :`${index === 4 ? "":"opacity-0"}`}`}
-            >
-              {letter}
-            </motion.div>
-          ))} */}
+        <div className="z-10 flex flex-col justify-end w-full items-end">
+        <div className="text-[150px] leading-[100px] font-bold tracking-tight text-tertiary">
+            NICK
+          </div>
+          <div className="text-[150px] leading-[100px] mt-7 font-bold tracking-tight text-tertiary">
+              SHUM
+          </div>
         </div>
       </div>
-    </div>
-    <motion.div className="h-[100dvh] absolute top-0 left-0 bg-white w-full flex justify-center items-center"
-    variants={slideDownLast}
-    initial="initial"
-    animate="enter"
-    exit="exit"
-    >
-      TEST
-    </motion.div>
+      <div className="z-0 absolute top-0 left-0 h-[100svh] w-full flex justify-center items-center">
+        <div className="leading-[10px] font-bold text-[800px] tracking-tighter text-[#B6b7B9] opacity-[24%]">
+          NS.
+        </div>
+      </div>
     </>
     
   );
--- src/sections/Loading.jsx
+++ src/sections/Loading.jsx
@@ -1,9 +1,89 @@
-import React from 'react'
-
+import React from "react";
+import {
+  motion,
+  useMotionValue,
+  useTransform,
+  AnimatePresence,
+  easeOut,
+} from "framer-motion";
+const firstName = ["N", "I", "C", "K", "S", "H", "U", "M", "."];
+const slideUpFirst = {
+  initial: {
+    y: 0,
+  },
+  enter: (index) => ({
+    y: -150,
+    transition: {
+      delay: index * 0.03 + 1,
+      duration: 1,
+      ease: [0.22, 1, 0.36, 1],
+    },
+  }),
+  exit: {
+    y: 0,
+  },
+};
+const slideUpSecond = {
+  initial: {
+    y: 0,
+  },
+  enter: (index) => ({
+    y: -150,
+    x: index === 0 ? 260 : index === 8 ? -360 : 0,
+    transition: {
+      delay: index * 0.03 + 1,
+      duration: 1,
+      ease: [0.22, 1, 0.36, 1],
+      x: {
+        delay: 1.9,
+        duration: 1,
+        ease: [0.68, -0.6, 0.32, 1.6],
+      },
+    },
+  }),
+  exit: {
+    y: 0,
+  },
+};
 const Loading = () => {
   return (
-    <div>Loading</div>
-  )
-}
+    <div className="relative z-0 h-[100dvh] w-[100dvw] flex flex-col justify-center items-center bg-secondary">
+      <div className="h-[164px] overflow-y-clip flex-col flex justify-start items-center">
+        <div className="text-primary uppercase flex font-bold text-[150px] leading-[150px]">
+          {firstName.map((letter, index) => (
+            <motion.div
+              key={index}
+              custom={index}
+              variants={slideUpFirst}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+            >
+              {letter}
+            </motion.div>
+          ))}
+        </div>
 
-export default Loading
+        <div className="text-primary uppercase flex font-bold text-[150px] leading-[150px]">
+          {firstName.map((letter, index) => (
+            <motion.span
+              key={index}
+              custom={index}
+              variants={slideUpSecond}
+              initial="initial"
+              animate="enter"
+              exit="exit"
+              className={`${
+                index === 0 || index === 4 || index === 8 ? "" : "opacity-0"
+              }`}
+            >
+              {letter}
+            </motion.span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
