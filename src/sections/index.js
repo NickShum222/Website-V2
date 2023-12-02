91afb0ca74cbaf756555e2b35c7461ba7d4c3566
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-12-02 12:10:23 -0800

    Initial project structure

---
+++ src/sections/index.js
@@ -0,0 +1,17 @@
+import Loading from "./Loading";
+import Landing from "./Landing";
+import About from "./About";
+import Projects from "./Projects";
+import Contact from "./Contact";
+import Experiences from "./Experiences";
+import Services from "./Services";
+
+export {
+  Loading,
+  Landing,
+  About,
+  Projects,
+  Contact,
+  Experiences,
+  Services,
+}
