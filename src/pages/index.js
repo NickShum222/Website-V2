263bf1264a0c9d0a9e5c65d77fc849d43ef28885
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-12-02 16:07:47 -0800

    More debugging

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,32 +4,9 @@ import { Loading, Landing, About } from "@/sections";
 import { styles } from "@/styles";
 
 export default function Home() {
-  const slideUpFirst = {
-    initial: {
-      y: 0,
-    },
-    enter: {
-      y: -150,
-      transition: {
-        delay: 1,
-        duration: 1,
-        ease: [0.22, 1, 0.36, 1],
-      },
-    },
-    exit: {
-      y: 0,
-    },
-  };
   return (
     <>
-    <motion.div 
-    variants={slideUpFirst}
-    initial="initial"
-    animate="enter"
-    exit="exit"
-    className="flex justify-center items-center w-full h-[100dvh]">
-      HELLO
-    </motion.div>
+    <Loading/>
     {/* <Loading/> */}
     </>
     // <div className="flex flex-col">
