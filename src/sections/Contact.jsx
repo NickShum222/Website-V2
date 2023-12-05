8fb09e51fcc4e34b21b9951c47bf642ba2df8ba0
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-12-05 00:48:27 -0800

    Update Contact page

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -5,9 +5,15 @@ import emailjs from "@emailjs/browser";
 import { useFormik } from "formik";
 import * as Yup from "yup";
 import { Input } from "@material-tailwind/react";
+import { 
+  FaLinkedin,
+  FaInstagram,
+  FaGithub,
+  FaIntercom,
+
+} from "react-icons/fa";
 
 const Contact = () => {
-  console.log(process.env.NEXT_PUBLIC_SERVICE_ID);
   const formik = useFormik({
     initialValues: {
       name: "",
@@ -28,10 +34,10 @@ const Contact = () => {
       try {
         emailjs
           .send(
-            process.env.REACT_APP_SERVICE_ID,
-            "template_i6xxiwi",
+            process.env.NEXT_PUBLIC_SERVICE_ID,
+            process.env.NEXT_PUBLIC_TEMPLATE_ID,
             values,
-            "wO24vRyimtsLwsozE"
+            process.env.NEXT_PUBLIC_PUBLIC_KEY
           )
           .then(() => {
             setSubmitting(false);
@@ -62,12 +68,12 @@ const Contact = () => {
           </div>
         </div>
       </div>
-      <div className="flex flex-row justify-between items-start w-full">
-        <div className="flex flex-col items-start w-[70%]">
-          <div className="text-[4.722vw] font-[700] text-secondary leading-[1.1] tracking-tighter">
-            Want to say hello? <br /> Feel free to reach out!
+      <div className="flex lg:flex-row flex-col justify-between items-start w-full">
+        <div className="flex flex-col items-start lg:w-[65%] w-full">
+          <div className="lg:text-[4.722vw] text-[5.305vw] w-full font-[700] text-secondary leading-[1.1] tracking-tighter mb-[2%]">
+            Want to say hello? <br className="lg:inline hidden" /> Feel free to reach out!
           </div>
-          <div className="text-[3.125vw] font-[500] text-grey2 leading-[1.2] tracking-tight">
+          <div className="lg:text-[3.125vw] text-[3.980vw] font-[500] text-grey2 leading-[1.2] tracking-tight mb-[4%]">
             I am looking for Summer 2024 internship opportunities.
           </div>
           <input
@@ -75,7 +81,7 @@ const Contact = () => {
             placeholder="Name"
             onChange={formik.handleChange}
             value={formik.values.name}
-            className="w-full appearance-none bg-transparent text-[2.083vw] font-[300] text-grey2 placeholder-grey2 leading-[1.2] tracking-tight border-b-[1px] py-[2%] px-[2%] border-solid border-grey4 focus:outline-none focus:border-grey2"
+            className="w-full appearance-none bg-transparent lg:text-[2.083vw] text-[2.985vw] font-[300] text-grey2 placeholder-grey2 leading-[1.2] tracking-tight border-b-[1px] py-[2%] px-[2%] border-solid border-grey4 focus:outline-none focus:border-grey2"
           />
           <div
             className={`mb-4 mt-1 lg:text-[16px] text-[14px] text-[#A31621]`}
@@ -87,7 +93,7 @@ const Contact = () => {
             placeholder="Email"
             onChange={formik.handleChange}
             value={formik.values.email}
-            className="w-full appearance-none bg-transparent text-[2.083vw] font-[300] text-grey2 placeholder-grey2 leading-[1.2] tracking-tight border-b-[1px] py-[2%] px-[2%] border-solid border-grey4 focus:outline-none focus:border-grey2"
+            className="w-full appearance-none bg-transparent lg:text-[2.083vw] text-[2.985vw] font-[300] text-grey2 placeholder-grey2 leading-[1.2] tracking-tight border-b-[1px] py-[2%] px-[2%] border-solid border-grey4 focus:outline-none focus:border-grey2"
           />
           <div
             className={`mb-4 mt-1 lg:text-[16px] text-[14px] text-[#A31621]`}
@@ -99,20 +105,45 @@ const Contact = () => {
             placeholder="Message"
             onChange={formik.handleChange}
             value={formik.values.message}
-            className="w-full appearance-none bg-transparent text-[2.083vw] font-[300] text-grey2 placeholder-grey2 leading-[1.2] h-[300px] tracking-tight border-b-[1px] py-[2%] px-[2%] border-solid border-grey4 focus:outline-none focus:border-grey2"
+            className="w-full appearance-none bg-transparent lg:text-[2.083vw] text-[2.985vw] font-[300] text-grey2 placeholder-grey2 leading-[1.2] lg:h-[300px] md:h-[150px] h-[125px] tracking-tight border-b-[1px] py-[2%] px-[2%] border-solid border-grey4 focus:outline-none focus:border-grey2"
           />
           <div
             className={`mb-4 mt-1 lg:text-[16px] text-[14px] text-[#A31621]`}
           >
             {formik.errors.message}
           </div>
-          <button
-            type="submit"
-            onClick={formik.handleSubmit}
-            className="text-primary font-[700] tracking-tight cursor-pointer text-[2.083vw] px-[7%] py-[0.25%] bg-tertiary"
-          >
-            SUBMIT
-          </button>
+          <div className="flex w-full justify-end">
+            <button
+              type="submit"
+              onClick={formik.handleSubmit}
+              className="text-center font-[700] w-full py-[2%] text-primary tracking-tight leading-[1.3] bg-tertiary rounded-[6px] lg:text-[2.083vw] text-[3.980vw]"
+            >
+              SUBMIT
+            </button>
+          </div>
+        </div>
+        <div className="flex flex-col items-end justify-end lg:w-[25%]  w-full gap-24">
+          <div className="flex flex-col items-start w-full gap-2">
+            <div className="font-[700] text-grey2 tracking-tight leading-[1.3] lg:text-[3.125vw] text-[5.305vw]">My Resume</div>
+            <div className="text-center font-[700] w-full py-[2%] text-primary tracking-tight leading-[1.3] bg-tertiary rounded-[6px] lg:text-[2.083vw] text-[3.980vw]">RESUME</div>
+          </div>
+          <div className="flex flex-col items-start w-full">
+          <div className="font-[700] text-grey2 tracking-tight leading-[1.3] lg:text-[3.125vw] text-[5.305vw] lg:mb-[8%] mb-[4%]">My Socials</div>
+          <div className="w-full flex lg:flex-col flex-row lg:items-start justify-start lg:gap-8  items-center">
+          <div className="flex lg:justify-start justify-center w-full items-center lg:gap-2 gap-1">
+            <FaLinkedin className="lg:text-[2.778vw]  text-[5.305vw] text-grey4"/>
+            <div className="text-grey4 lg:text-[2.083vw]  text-[5.305vw] font-[500] tracking-tight">LinkedIn</div>
+          </div>
+          <div className="flex lg:justify-start justify-center w-full items-center lg:gap-2 gap-1">
+            <FaInstagram className="lg:text-[2.778vw]  text-[5.305vw] text-grey4"/>
+            <div className="text-grey4 lg:text-[2.083vw]  text-[5.305vw] font-[500] tracking-tight">Instagram</div>
+          </div>
+          <div className="flex lg:justify-start justify-center w-full items-center lg:gap-2 gap-1">
+            <FaGithub className="lg:text-[2.778vw]  text-[5.305vw] text-grey4"/>
+            <div className="text-grey4 lg:text-[2.083vw]  text-[5.305vw] font-[500] tracking-tight">Github</div>
+          </div>
+          </div>
+          </div>
         </div>
       </div>
     </div>
