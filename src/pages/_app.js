ba0d929b2fb09bf66fd544f2b7b239c6262bb9d2
NickShum <115332882+NickShum222@users.noreply.github.com>
2023-12-12 23:17:37 -0800

    Added page transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,15 +8,37 @@ const satoshi = localFont({
   weight: "variable",
 });
 import { SmoothScroll } from "@/components";
-import { SpeedInsights } from '@vercel/speed-insights/next';
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { motion, AnimatePresence } from "framer-motion";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
   return (
     <SmoothScroll>
-      <main className={`${satoshi.variable} font-sans`}>
-        <Component {...pageProps} />
-        <SpeedInsights />
-      </main>
+      <AnimatePresence mode="wait">
+        <motion.div
+          className={`${satoshi.variable} font-sans`}
+          key={router.pathname}
+        >
+          <Component {...pageProps} />
+          <SpeedInsights />
+          <motion.div
+            className="absolute top-0 left-0 w-full h-[100dvh] bg-tertiary origin-bottom"
+            initial={{ scaleY: 0 }}
+            animate={{ scaleY: 0 }}
+            exit={{ scaleY: 1 }}
+            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+          ></motion.div>
+          <motion.div
+            className="absolute top-0 left-0 w-full h-[100dvh] bg-tertiary origin-top"
+            initial={{ scaleY: 1 }}
+            animate={{ scaleY: 0 }}
+            exit={{ scaleY: 0 }}
+            transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+          ></motion.div>
+        </motion.div>
+      </AnimatePresence>
     </SmoothScroll>
   );
 }
