9f7e40876485f3fc3b11978b44ba29e4f779761b
NickShum <115332882+NickShum222@users.noreply.github.com>
2024-01-21 13:29:39 -0500

    Changed footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,19 +2,15 @@ const index = () => {
   return (
     <div
       className={
-        "w-full flex justify-center flex-col items-center dark:bg-secondary bg-primary pt-8 pb-4"
+        "w-full flex justify-center flex-col items-center dark:bg-secondary bg-primary pt-4  lg:px-[6%] md:px-[64px] px-[24px]"
       }
     >
-      <a
-        href="https://github.com/NickShum222/Website-V2"
-        target={"_blank"}
-        className="dark:text-primary text-grey2 sm:text-[18px] text-[14px] hover:text-tertiary duration-150 transition-all ease-in font-[500]"
-      >
-        Designed & Developed by Nick Shum
-      </a>
-      <p className="dark:text-primary text-grey2 sm:text-[18px] text-[14px] font-[500]">
-        © Copyright 2024.
+      <p className="select-none text-opacity-70 w-full flex justify-center border-t-[1px] border-tertiary sm:py-6 py-3 border-solid border-opacity-50 dark:text-primary text-grey2 sm:text-[18px] text-[14px] hover:text-tertiary duration-150 transition-all ease-in font-[400]">
+        Designed & Built by Nick Shum
       </p>
+      {/* <p className="dark:text-primary text-grey2 sm:text-[18px] text-[14px] font-[500]">
+        © Copyright 2024.
+      </p> */}
     </div>
   );
 };
