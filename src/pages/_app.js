14cbff0d8999fa68e82e1aa290f590690e019565
Nick <nickshum222@gmail.com>
2025-01-07 18:00:23 -0500

    Fixed page transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -52,6 +52,13 @@ export default function App({ Component, pageProps }) {
               <SpeedInsights />
               <Analytics />
             </div>
+            <motion.div
+              className="fixed z-[200] top-0 left-0 w-full h-[100dvh] bg-tertiary origin-bottom"
+              initial={{ scaleY: 0 }}
+              animate={{ scaleY: 0 }}
+              exit={{ scaleY: 1 }}
+              transition={{ duration: 1, ease: [0.22, 1, 0.36, 1] }}
+            ></motion.div>
             {loaded && (
               <motion.div
                 className="fixed z-[200] top-0 left-0 w-full h-[100dvh] bg-tertiary origin-top"
